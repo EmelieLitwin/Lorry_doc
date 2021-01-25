cf0d21fcebea76293930eda540d6989b578b9b97
Emelie Litwin <emelielitwin@gmail.com>
2021-01-25 21:56:06 +0100

    Some basic setup

---
+++ LorryDoc/server.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const server = http.createServer(app);
+const path = require("path");
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client/index.html');
+})
+
+
+//Declaration of static files (CSS) to use
+app.use(express.static(path.resolve(__dirname, "client")));
+
+
+server.listen(
+    process.env.PORT || 3000,
+    function () {
+        var addr = server.address();
+        console.log(
+            "All ready! Server listening at port:" + addr.port
+        );
+    }
+);
+
