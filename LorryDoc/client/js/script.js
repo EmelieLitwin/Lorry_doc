2140a2898796ee04d04469fdc967e9d09d02fa97
Nathalie_Lindqvist <nathalielindqvist@gmail.com>
2021-01-29 13:08:12 +0100

    Test create element with data from database

--- LorryDoc/client/js/script.js
+++ LorryDoc/client/js/script.js
@@ -10,4 +10,26 @@ success: function(result){
         console.log(value+index)
     });
     console.log(result)
+
+    // Get element "See"-button
+    var seeBtn = document.getElementById('btn-see');
+
+    // Add eventListener to "See"-button
+    seeBtn.addEventListener('click', function(){
+
+        // Get selected div for later placement
+        var symptoms2 = document.getElementById('symptoms2');
+
+        // Create new button element, add result from database as innerHTML, set attribute
+        var smokeBtn = document.createElement('button');
+        smokeBtn.innerHTML = result[0];
+        smokeBtn.setAttribute('id', smokeBtn);
+
+        // Append newly created button to div
+        symptoms2.appendChild(smokeBtn);
+
+    });
+    
+
+
 }});
