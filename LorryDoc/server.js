6ef52c963f701583b7fdfd851321dee647e20bd0
Nathalie_Lindqvist <nathalielindqvist@gmail.com>
2021-02-01 12:55:24 +0100

    Fetch + display data from rest of tables

--- LorryDoc/server.js
+++ LorryDoc/server.js
@@ -40,7 +40,7 @@ con.connect (function(err) {
 });
 
 
-// Endpoint for fetched data
+// Endpoint for fetched data table "Symptoms2"
 app.get('/getdata', (req, res) => {
     // MySQL query
     var queryString = "SELECT Description FROM Symptoms2";
@@ -55,12 +55,82 @@ app.get('/getdata', (req, res) => {
 
                 // Focus on the row data, push onto previously empty array
                 symptoms2Table.push(entry.Description);
-                console.log(entry.Description);
             });
 
             // Respond with JSON result
             res.json(symptoms2Table);
             console.log(symptoms2Table);
+        }
+    });
+        
+});
+
+
+// Endpoint for fetched data table "Symptoms2"
+app.get('/getnextdata', (req, res) => {
+    // MySQL query
+    var queryString1 = "SELECT Description FROM Symptoms3";
+
+    con.query(queryString1, function (err, rows, fields) {
+        if (err) throw err;
+        else {
+            var symptoms3Table = [];
+
+            // Loop query results
+            rows.forEach(function(entry) {
+
+                // Focus on the row data, push onto previously empty array
+                symptoms3Table.push(entry.Description);
+            });
+
+            // Respond with JSON result
+            res.json(symptoms3Table);
+            console.log(symptoms3Table);
+        }
+    });
+        
+});
+
+// Endpoint for fetched data table "Solution"
+app.get('/getlastdata', (req, res) => {
+    // MySQL query
+    // var queryString2 = "SELECT Suggestion FROM Solution";
+
+    // con.query(queryString2, function (err, rows, fields) {
+    //     if (err) throw err;
+    //     else {
+    //         var suggestion = [];
+
+    //         // Loop query results
+    //         rows.forEach(function(entry) {
+
+    //             // Focus on the row data, push onto previously empty array
+    //             suggestion.push(entry.Description);
+    //         });
+
+    //         // Respond with JSON result
+    //         res.json(suggestion);
+    //         console.log(suggestion);
+    //     }
+    // });
+
+    var queryString3 = "SELECT Description FROM Solution";
+
+    con.query(queryString3, function (err, rows, fields) {
+        if (err) throw err;
+        else {
+            var resolution = [];
+
+            // Loop query results
+            rows.forEach(function(entry) {
+
+                // Focus on the row data, push onto previously empty array
+                resolution.push(entry.Description);
+            });
+
+            // Respond with JSON result
+            res.json(resolution);
+            console.log(resolution);
         }
     });
         
