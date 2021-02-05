d476cf38ccce727aa8e9615ad3043c0c7ee63b80
Emelie Litwin <emelielitwin@gmail.com>
2021-02-05 19:39:58 +0100

    -Linked start page -Saves mysql table rows as object arrays, i.e. every column not just description -Creates buttons with for-loops

--- LorryDoc/server.js
+++ LorryDoc/server.js
@@ -7,7 +7,7 @@ const mysql = require('mysql');
 
 // Present root file
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/client/index.html');
+    res.sendFile(__dirname + '/client/start.html');
 })
 
 // Path to client folder
@@ -43,7 +43,7 @@ con.connect (function(err) {
 // Endpoint for fetched data table "Symptoms2"
 app.get('/getdata', (req, res) => {
     // MySQL query
-    var queryString = "SELECT Description FROM Symptoms2";
+    var queryString = "SELECT idSymptoms2, Description, Symptoms_idSymptoms FROM Symptoms2";
 
     con.query(queryString, function (err, rows, fields) {
         if (err) throw err;
@@ -54,11 +54,12 @@ app.get('/getdata', (req, res) => {
             rows.forEach(function(entry) {
 
                 // Focus on the row data, push onto previously empty array
-                symptoms2Table.push(entry.Description);
+                symptoms2Table.push({idSymptoms2:entry.idSymptoms2, description:entry.Description, symptoms_id:entry.Symptoms_idSymptoms});
             });
 
             // Respond with JSON result
             res.json(symptoms2Table);
+            console.log('symptom2');
             console.log(symptoms2Table);
         }
     });
@@ -69,7 +70,7 @@ app.get('/getdata', (req, res) => {
 // Endpoint for fetched data table "Symptoms2"
 app.get('/getnextdata', (req, res) => {
     // MySQL query
-    var queryString1 = "SELECT Description FROM Symptoms3";
+    var queryString1 = "SELECT idSymptoms3, Description, Symptoms2_idSymptoms2, Solution_idSymptoms31 FROM Symptoms3";
 
     con.query(queryString1, function (err, rows, fields) {
         if (err) throw err;
@@ -80,11 +81,12 @@ app.get('/getnextdata', (req, res) => {
             rows.forEach(function(entry) {
 
                 // Focus on the row data, push onto previously empty array
-                symptoms3Table.push(entry.Description);
+                symptoms3Table.push({description:entry.Description, idSymptoms3:entry.idSymptoms3, symptoms2_id:entry.Symptoms2_idSymptoms2, solution_id:entry.Solution_idSymptoms31});
             });
 
             // Respond with JSON result
             res.json(symptoms3Table);
+            console.log('symptom3  ')
             console.log(symptoms3Table);
         }
     });
@@ -94,27 +96,7 @@ app.get('/getnextdata', (req, res) => {
 // Endpoint for fetched data table "Solution"
 app.get('/getlastdata', (req, res) => {
     // MySQL query
-    // var queryString2 = "SELECT Suggestion FROM Solution";
-
-    // con.query(queryString2, function (err, rows, fields) {
-    //     if (err) throw err;
-    //     else {
-    //         var suggestion = [];
-
-    //         // Loop query results
-    //         rows.forEach(function(entry) {
-
-    //             // Focus on the row data, push onto previously empty array
-    //             suggestion.push(entry.Description);
-    //         });
-
-    //         // Respond with JSON result
-    //         res.json(suggestion);
-    //         console.log(suggestion);
-    //     }
-    // });
-
-    var queryString3 = "SELECT Description FROM Solution";
+    var queryString3 = "SELECT idSolution, Suggestion, Description, idSymptoms3 FROM Solution";
 
     con.query(queryString3, function (err, rows, fields) {
         if (err) throw err;
@@ -125,11 +107,12 @@ app.get('/getlastdata', (req, res) => {
             rows.forEach(function(entry) {
 
                 // Focus on the row data, push onto previously empty array
-                resolution.push(entry.Description);
+                resolution.push({description:entry.Description, idSymptoms3:entry.idSymptoms3, idSolution:entry.idSolution, suggestion:entry.Suggestion});
             });
 
             // Respond with JSON result
             res.json(resolution);
+            console.log('resolution ')
             console.log(resolution);
         }
     });
